e2f1eeee934dcf412b71704be21ef40502852856
CarlCloud <131357597+CarlCloud@users.noreply.github.com>
2025-02-16 23:10:13 +0800

    Create infer_sft_kvcache.py
    更新KV Cache及其相关的新输入构建方法

---
+++ minilm2/llm/infer_sft_kvcache.py
@@ -0,0 +1,187 @@
+import math
+import queue
+import torch
+from tqdm import tqdm
+from torch.nn import functional as F
+from model import NGPT
+from config import Config as config
+from tokenizers import Tokenizer # type: ignore
+from threading import Thread
+
+
+def build_context(history: list[tuple[str, str]], tokenizer: Tokenizer,
+                max_length: int, system_prompt: str | None = None) -> torch.Tensor:
+    ids = []
+    human_prefix_ids = tokenizer.encode(config.HUMAN_PREFIX).ids
+    ai_prefix_ids = tokenizer.encode(config.AI_PREFIX).ids
+    separator_ids = tokenizer.encode("\n" * 3).ids
+
+    text = history[-1]
+    ids += human_prefix_ids + tokenizer.encode(text[0]).ids + separator_ids
+    ids += ai_prefix_ids + tokenizer.encode(text[1]).ids
+
+    return torch.LongTensor(ids).unsqueeze(0)
+
+
+def append_history(history: list[tuple[str, str]], role: str, text: str) -> list[tuple[str, str]]:
+    if role == "human":
+        history.append((text, ""))
+    else:
+        history[-1] = (history[-1][0], text)
+    return history
+
+
+result_queue = queue.Queue()
+def async_input():  # 用于修复调试时concole无法单独开进程input的问题
+    def worker():
+        tmp = sys.stdin.read().strip()
+        result_queue.put(tmp)
+
+    t = Thread(target=worker())
+    t.start()
+    t.join()
+
+    return result_queue.get()
+
+
+def get_input(history):
+    text = async_input() + "\n"
+
+    if text=="!clear":
+        history = []
+        print("History cleared.")
+
+    if text.startswith("!top_p"):
+        try:
+            top_p = float(text.split()[1])
+            if top_p<=0 or top_p>1:
+                raise ValueError
+            train_config['top_p'] = top_p
+            print(f"top_p set to {top_p}.")
+        except (ValueError, IndexError):
+            print("top_p value should be a float between 0 and 1. Usage: !top_p x.xx")
+
+    if text.startswith("!temperature"):
+        try:
+            temperature = float(text.split()[1])
+            if temperature<=0 or temperature>2:
+                raise ValueError
+            train_config['temperature'] = temperature
+            print(f"temperature set to {temperature}.")
+        except (ValueError, IndexError):
+            print("temperature value should be a float between 0 and 2. Usage: !temperature x.xx")
+
+    if text.startswith("!context"):
+        for i in build_context(
+                history,
+                tokenizer,
+                train_config['max_length'],
+                system_prompt=train_config.get("system_prompt")).squeeze():
+            print(tokenizer.id_to_token(i.item()), end="", flush=True)
+        print()
+
+    if text.startswith("!history"):
+        print(history)
+
+    # 加入历史记录
+    history = append_history(history, "human", text)
+
+    return text, history
+
+
+
+if __name__ == '__main__':
+    import sys
+    import os
+    import json
+    # if len(sys.argv) < 2:
+    #     print('Usage: python -m minilm2.llm.eval_pretrained <config_path>')
+    #     exit(1)
+    # config_path = sys.argv[1]
+    current_dir = os.path.dirname(__file__)
+    config_path = os.path.join(os.path.dirname(os.path.dirname(current_dir)), "models/pretrain/pretrain_ngpt.json")
+    config_dir = os.path.dirname(config_path) # 配置文件路径
+    train_config = json.load(open(config_path))
+
+    # 加载tokenizer并获取词表大小
+    print("Loading tokenizer...")
+    tokenizer = Tokenizer.from_file(os.path.join(config_dir, train_config['tokenizer_path']))
+    vocab_size = tokenizer.get_vocab_size()
+    print(f"==> Vocab size: {vocab_size}")
+
+    # 根据配置文件创建模型
+    model_type = train_config["model"]
+    print(f"Loading {model_type} model...")
+    model: torch.nn.Module
+    if model_type == "NGPT":
+        model = NGPT(
+            vocab_size=vocab_size,
+            dim=train_config['model_dim'],
+            max_length=train_config['max_length'],
+            n_heads=train_config['num_heads'],
+            n_blocks=train_config['num_layers'],
+            dropout=0, # 推理时不使用dropout
+            device="cuda:0",
+        )
+    elif model_type == "RWKV7":
+        model = RWKV7(
+            vocab_size=2 ** math.ceil(math.log2(vocab_size)), # 确保vocab_size为2的幂
+            dim=train_config['model_dim'],
+            n_blocks=train_config['num_layers'],
+            max_lr=train_config['max_learning_rate']
+        )
+    # 统计参数量
+    params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print(f"==> Number of parameters: {params / 1e6:.2f}M")
+    # 加载已有的检查点
+    if train_config['checkpoint_file']:
+        checkpoint_path = os.path.join(config_dir, train_config['checkpoint_file'])
+        print(f"==> Loading checkpoint from {checkpoint_path}, step={train_config['checkpoint_step']}")
+        model.load_state_dict(torch.load(checkpoint_path, weights_only=True))
+
+    # 将模型移动到显存并编译以加速推理
+    model.to(config.DEVICE)
+    # model.compile(fullgraph=True) # 本地console请注释掉
+    model.eval()
+
+    torch.set_float32_matmul_precision('high') # 调整精度以加速推理
+    history: list[tuple[str, str]] = []
+    print("Use '!clear' to clear history. Use '!top_p x.xx' to adjust top_p. Use '!temperature x.xx' to adjust temperature.")
+    while True:
+        text, history = get_input(history)
+        # 构建输入
+        input_ids = build_context(history, tokenizer, train_config['max_length'], train_config.get("system_prompt"))
+        input_ids = input_ids.to(config.DEVICE)
+        # 推理
+        response = ""
+        n_blankline = 0
+        position_idx = input_ids.numel()
+        with torch.no_grad():
+            while True:
+                try:
+                    output = model(input_ids, position_idx)
+                    logits = F.softmax(output[0][-1] / train_config['temperature'], dim=-1)
+                    # 采样输出，取概率最高的n个进行加权随机采样
+                    probs, indices = logits.topk(round(vocab_size * train_config['top_p']))
+                    sample = torch.multinomial(probs, 1)
+                    token_id = indices[sample]
+                    prob = probs[sample].item()
+
+                    # input_ids = torch.cat([input_ids, token_id.unsqueeze(0)], dim=1)[:, -train_config['max_length']:] # 自回归生成
+                    token = tokenizer.id_to_token(token_id.item())
+                    input_ids = token_id.unsqueeze(0)
+                    position_idx += 1
+                    if token == "\n":
+                        n_blankline += 1
+                        if n_blankline >= 3:
+                            break
+                    else:
+                        n_blankline = 0
+                    confidence_level = round(prob ** 0.5 * 16)  # 开方以增大低概率时的颜色差异
+                    print(f"\033[1;38;5;{confidence_level + 239}m{token}\033[0m", end="", flush=True)
+                    response += token
+                except KeyboardInterrupt:
+                    print("\033[0m")
+                    break
+        # 加入历史记录
+        history = append_history(history, "ai", response.strip())
